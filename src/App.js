c71aeccd818dfc906f31914d3ade7d1a230487b2
Ethan <ethan@Ethans-MacBook-Pro-2.local>
2021-07-26 13:25:15 -0700

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import '@fontsource/roboto';
-import './App.css';
+import logo from "./logo.svg";
+import "@fontsource/roboto";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <h1>Routing Test</h1>
+        </Route>
+        <Route exact path="/About">
+          <h1>Routing Test 1</h1>
+        </Route>
+        <Route exact path="/Projects">
+          <h1>Routing Test 2</h1>
+        </Route>
+        <Route exact path="/Experience">
+          <h1>Routing Test 3</h1>
+        </Route>
+        <Route exact path="/Clubs">
+          <h1>Routing Test 4</h1>
+        </Route>
+        <Route exact path="/Blogs">
+          <h1>Routing Test 5</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
